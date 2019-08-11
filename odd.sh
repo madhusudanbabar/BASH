3adcd623923663ecec2be2e939a9f0679f127fb3
madhusudan babar <madhusudanbabar@gmail.com>
2019-08-11 17:39:45 +0530

    fixed issues

--- odd.sh
+++ odd.sh
@@ -1,10 +1,11 @@
-!#/bin/bash 
-for i in `seq 1 100`
+#!/usr/bin/bash
+for i in `seq 1 20`
 do
-	if [ `expr $i / 2 ` -eq 0 ] then
-		echo $i;
+	if [ `expr $i % 2` -eq 0 ];
+	then 
+		continue
 	else
-		continue;
-	fi
-done
-	
+		echo "$i"
+		fi
+	done
+
