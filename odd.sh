32b3f05fbcbe544a6cf1b37f360802b1e0556c40
madhusudan babar <madhusudanbabar@gmail.com>
2019-08-11 17:50:08 +0530

    added exception handling

--- odd.sh
+++ odd.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/bash
-for i in `seq $1 $2`
-do
-	if [ `expr $i % 2` -eq 0 ];
-	then 
-		continue
-	else
-		echo "$i"
+if [ $# -eq 2 ];
+then
+	for i in `seq $1 $2`
+	do
+		if [ `expr $i % 2` -eq 0 ];
+		then 
+			continue
+		else
+			echo "$i"
 		fi
 	done
+else
+	echo "error you must specify the start and endpoint"
 
+fi
